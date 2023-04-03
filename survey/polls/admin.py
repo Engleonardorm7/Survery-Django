e27bea0a748e926971ce946d19fe7d3064390618
Leonardo Rodriguez <leonardorm7@hotmail.com>
2023-04-02 22:34:32 -0500

    adding the option to create questions and the choices in the admin mode, adding css backgrounds and styles

--- survey/polls/admin.py
+++ survey/polls/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra=3
+
+# Personaliza como se ven los modelos en el administrador
+class QuestionAdmin(admin.ModelAdmin):
+    fields=["pub_date","question_text"]
+    inlines=[ChoiceInLine] #incluir las respuestas cuando se crea la pregunta
+    list_display=["question_text","pub_date","was_published_recently"]
+    list_filter=["pub_date","question_text"]
+    search_fields=["question_text"]
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
